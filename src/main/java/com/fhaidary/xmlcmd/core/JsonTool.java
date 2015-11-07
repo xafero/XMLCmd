c67b433cfe0c306efc230fd5973d6852e4f9db4d
xafero <georg.berendt@uni-oldenburg.de>
2015-11-07 02:10:32 +0100

    XML to JSON conversion now even embeds attributes, namespaces, and child trees

--- src/main/java/com/fhaidary/xmlcmd/core/JsonTool.java
+++ src/main/java/com/fhaidary/xmlcmd/core/JsonTool.java
@@ -10,9 +10,14 @@ import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.stream.JsonWriter;
 
 public final class JsonTool {
+
+	private static final JsonParser parser = new JsonParser();
 
 	public static void convert2Json(String file) throws IOException, XMLStreamException {
 		// Set it up...
@@ -33,25 +38,48 @@ public final class JsonTool {
 
 	private static void convert2Json(XMLStreamReader xml, JsonWriter json) throws XMLStreamException, IOException {
 		for (int event = xml.getEventType();; event = xml.next()) {
-			String localName;
+			String itemName;
 			switch (event) {
 			case XMLStreamReader.START_DOCUMENT:
 				json.beginObject();
 				break;
 			case XMLStreamReader.START_ELEMENT:
-				localName = xml.getLocalName();
-				json.name(localName);
+				String fix = xml.getPrefix();
+				String name = xml.getLocalName();
+				itemName = fix == null || fix.isEmpty() ? name : fix + "_" + name;
+				json.name(itemName);
 				json.beginObject();
+				for (int i = 0; i < xml.getAttributeCount(); i++) {
+					String attrFix = xml.getAttributePrefix(i);
+					String attrName = xml.getAttributeLocalName(i);
+					String attrVal = xml.getAttributeValue(i);
+					attrName = attrFix == null || attrFix.isEmpty() ? attrName : attrFix + "_" + attrName;
+					json.name(attrName);
+					json.value(attrVal);
+				}
+				for (int i = 0; i < xml.getNamespaceCount(); i++) {
+					String nsName = xml.getNamespacePrefix(i);
+					nsName = nsName == null ? "xmlns" : "xmlns_" + nsName;
+					String nsVal = xml.getNamespaceURI(i);
+					json.name(nsName);
+					json.value(nsVal);
+				}
 				break;
 			case XMLStreamReader.CHARACTERS:
 				String txt = xml.getText().trim();
 				if (!txt.isEmpty()) {
-					json.name("text");
-					json.value(txt);
+					try {
+						JsonElement elem = parser.parse('{' + txt + '}');
+						json.name("#sub");
+						json.jsonValue(elem.toString());
+					} catch (JsonSyntaxException jse) {
+						json.name("#txt");
+						json.value(txt);
+					}
 				}
 				break;
 			case XMLStreamReader.END_ELEMENT:
-				localName = xml.getLocalName();
+				itemName = xml.getLocalName();
 				json.endObject();
 				break;
 			case XMLStreamReader.END_DOCUMENT:
